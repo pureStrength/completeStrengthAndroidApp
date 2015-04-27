f9466bac1e7c661cff189f2e50bcb875789f78b8
Kyle Mera <kmera1164@yahoo.com>
2015-04-27 15:23:50 -0400

    Updated configured ip address
    Updated configured ip address to be a machine independent value

--- app/src/main/java/com/completeconceptstrength/config/LocalServer.java
+++ app/src/main/java/com/completeconceptstrength/config/LocalServer.java
@@ -4,5 +4,5 @@ package com.completeconceptstrength.config;
  * Created by Kyle on 4/25/2015.
  */
 public class LocalServer {
-    public final static String IP_ADDRESS = "192.168.1.69:8080";
+    public final static String IP_ADDRESS = "10.0.2.2:8080";
 }
