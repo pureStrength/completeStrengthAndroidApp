22994b893e2ad3c1ef68c12b7748c15d71ebe02f
jessmay <jessica.may.cs@knights.ucf.edu>
2015-11-28 17:55:47 -0500

    More additions for notifications service

--- app/src/main/java/com/completeconceptstrength/application/MyGcmListenerService.java
+++ app/src/main/java/com/completeconceptstrength/application/MyGcmListenerService.java
@@ -6,4 +6,5 @@ import com.google.android.gms.gcm.GcmListenerService;
  * Created by Jessica on 11/28/2015.
  */
 public class MyGcmListenerService extends GcmListenerService {
+
 }
--- app/src/main/java/com/completeconceptstrength/application/MyInstanceIDListenerService.java
+++ app/src/main/java/com/completeconceptstrength/application/MyInstanceIDListenerService.java
@@ -1,9 +1,34 @@
 package com.completeconceptstrength.application;
 
+import android.content.Intent;
+
+import com.completeconceptstrength.activity.RegistrationActivity;
 import com.google.android.gms.iid.InstanceIDListenerService;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.iid.InstanceID;
+import com.google.android.gms.iid.InstanceIDListenerService;
 /**
  * Created by Jessica on 11/28/2015.
  */
 public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+    private static final String TAG = "MyInstanceIDLS";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startService(intent);
+    }
 }
