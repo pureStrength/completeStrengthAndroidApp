01f89ace930ed4c5897797b3b86eab96d6961867
jessmay <jessica.may.cs@knights.ucf.edu>
2015-11-29 18:10:33 -0500

    ORMs displaying on Athlete profile views

--- app/src/main/java/com/completeconceptstrength/activity/ViewProfileActivity.java
+++ app/src/main/java/com/completeconceptstrength/activity/ViewProfileActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.completeconceptstrength.R;
@@ -13,6 +15,11 @@ import com.completeconceptstrength.application.GlobalContext;
 import org.apache.http.HttpResponse;
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import completeconceptstrength.model.exercise.impl.OneRepMaxChart;
 import completeconceptstrength.model.user.impl.Athlete;
 import completeconceptstrength.model.user.impl.AthleteProfile;
 import completeconceptstrength.model.user.impl.User;
@@ -107,9 +114,60 @@ public class ViewProfileActivity extends AppCompatActivity {
         TextView nameTV = (TextView) findViewById(R.id.athleteName);
         TextView emailTV = (TextView) findViewById(R.id.athleteEmail);
         TextView orgTV = (TextView) findViewById(R.id.organization);
+        TableLayout ORMTable = (TableLayout) findViewById(R.id.ORMTable);
 
         nameTV.setText(profileUser.getFirstName() + " " + profileUser.getLastName());
         emailTV.setText(profileUser.getEmail());
         orgTV.setText(profileUser.getOrganization());
+
+        List<OneRepMaxChart> ORMs = profileAthlete.getMostRecentOneRepMaxes();
+
+        for(OneRepMaxChart O : ORMs){
+            TableRow tr = new TableRow(this);
+
+            TextView liftName = new TextView(this);
+            TextView value = new TextView(this);
+            TextView dateUpdated = new TextView(this);
+
+            liftName.setText(O.getLiftName());
+            liftName.setTextSize(18);
+            liftName.setPadding(0, 0, 40, 0);
+
+            String ORMvalue = Integer.toString(O.getMostRecentOneRepMax().getValue());
+
+            if(ORMvalue != null) {
+                value.setText(ORMvalue + " lbs");
+                value.setTextSize(18);
+                value.setPadding(0, 0, 40, 0);
+
+                Date updateDate = O.getMostRecentOneRepMax().getDate();
+
+                SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM d");
+
+                dateUpdated.setText("on " + sdf.format(updateDate));
+                dateUpdated.setTextSize(18);
+            }
+
+            tr.addView(liftName);
+            tr.addView(value);
+            tr.addView(dateUpdated);
+
+            TableLayout.LayoutParams tableRowParams=
+                    new TableLayout.LayoutParams
+                            (TableLayout.LayoutParams.WRAP_CONTENT,TableLayout.LayoutParams.WRAP_CONTENT);
+
+            int leftMargin=10;
+            int topMargin=10;
+            int rightMargin=10;
+            int bottomMargin = 2;
+
+            tableRowParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+
+            tr.setLayoutParams(tableRowParams);
+
+            ORMTable.addView(tr);
+        }
+
+
     }
 }
